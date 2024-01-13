21394d9a5e7a4a0b22ea0c79077bd843e14c48ee
SlobodanDodic <slobodandodic@gmail.com>
2024-01-13 19:51:01 +0100

    vanilla js - t3

--- T3/script.js
+++ T3/script.js
@@ -1,68 +1,134 @@
+// document.write("<p>" + "Happy birthday dear" + " " + name + "</p>");
 // Zadatak 1:
+document.getElementById("resultBtn1").addEventListener("click", function () {
+  let users = [];
 
-// Napisati funkciju koja prima par korisnicko ime lozinka u obliku:
+  function user(input) {
+    let inputSplit = input.split("|");
+    users.push(inputSplit);
+  }
 
-// korisnicko_ime|lozinka
+  let userM = "marko|mark123";
+  let userS = "steva|stev123";
 
-// funkcija treba da razdvoji korisnicko ime i lozinku po delimiteru ("|") i da doda u listu.
-// Pozvati funkciju za nekoliko razlicitih parova korisnicko ime i lozinka i zatim ih sve ispisati u obliku:
+  user(userM);
+  user(userS);
 
-// Korisnicko ime je : pera
-// lozinka je : peric
+  for (let i in users) {
+    alert("Korisnicko ime je: " + users[i][0]);
+    alert("Lozinka je: " + users[i][1]);
+    console.log(users[i]);
+  }
 
-// Korisnicko ime je : marko
-// Lozinka je : markovic
+  console.log(users);
+});
 
-// Napomena par korisnicko ime i lozinka cuvati u listi koja je element liste koja cuva sve parove korisnicko ime i lozinka.
+// Zadatak 2:
+document.getElementById("resultBtn2").addEventListener("click", function () {
+  let prvi = "abcdef";
+  let drugi = "ghijkl";
 
-// Zadatak 2
-// Napisati funkciju koja za dva unesena stringa formira i ispisuje novi string koji se sastoji od
-// dva puta ponovljena prva tri karaktera iz prvog stringa i poslednja tri karaktera drugog stringa.
+  function newString(str1, str2) {
+    let strPrvi = str1.substring(0, 3);
+    let strDrugi = str2.substring(str2.length - 3);
+    alert(strPrvi + strPrvi + strDrugi);
+  }
 
-// Primer izvršavanja programa:
-// prvi string: abcdef
-// drugi string: ghijkl
-// abcabcjkl
+  newString(prvi, drugi);
+});
 
-// Zadatak 3
-// Napisati program koji formira akronim za zadatu frazu. Akronim se sastoji od kapitalizovanih
-// prvih slova reči u frazi. Na primer RAM je akronim za frazu „random access memory“.
+// Zadatak 3:
+document.getElementById("resultBtn3").addEventListener("click", function () {
+  function acronym(param) {
+    let firstLetters = param.split(" ");
+    let letters = "";
 
-// Primer izvršavanja programa:
-// fraza: random access memory
-// akronim za unetu frazu je: RAM
+    for (let i in firstLetters) {
+      letters += firstLetters[i][0];
+    }
+    return letters.toUpperCase();
+  }
+
+  let result = acronym("random access memory");
+  alert(result);
+});
 
 // Zadatak 4:
-// Data je struktura:
+document.getElementById("resultBtn4").addEventListener("click", function () {
+  let ocene = [8, 7, 6, 9, 8, 10, 8];
+  let ime_prez = ["Marko", "Markovic"];
 
-// var ocene = [8, 7, 6, 9, 8, 10, 8];
-// var ime_prez = ["Marko", "Markovic"];
+  function averageRate(str, user) {
+    let average = 0;
 
-// Napraviti funkciju koja prima dva parametra, prvi je niz ocena, drugi sadrzi ime i prezime.
-// Funkcija treba da izracuna prosek ocena datog studenta i da ispise njegovo ime i prezime.
-// Npr: "Student Marko Markovic ima prosek 8.7..."
+    for (let i = 0; i < str.length; i++) {
+      average += str[i];
+    }
+    let mid = average / str.length;
+
+    alert(`Student ${user[0]} ${user[1]} ima prosek ${mid}`);
+  }
+
+  averageRate(ocene, ime_prez);
+});
 
 // Zadatak 5:
-// U datoj strukturi se nalazi lista drzava sa svojim kodovima. U drugoj strukturi se nalaze gradovi sa kodovima odredjene drzave.
-// Ispisati sve gradove i na osnovu koda drzave ispisati i njeno skraceno ime.
-// Npr: Novi Sad - RS, London - UK, Madrid - ESP
+document.getElementById("resultBtn5").addEventListener("click", function () {
+  let drzave = {
+    1: "RS",
+    2: "RU",
+    3: "UK",
+    4: "ESP",
+  };
+  let gradovi = [
+    ["London", 3],
+    ["Novi Sad", 1],
+    ["Moskva", 2],
+    ["Madrid", 4],
+  ];
 
-// var drzave = { 1 : "RS" , 2 : "RU" , 3 : "UK", 4 : "ESP" };
-// var gradovi = [ ["London", 3], ["Novi Sad", 1], ["Moskva", 2], ["Madrid", 4]];
+  function cityCodes(states, cities) {
+    for (let i in cities) {
+      let city = cities[i];
+      let state = states[city[1]];
+
+      alert(city[0] + " - " + state);
+    }
+  }
+
+  cityCodes(drzave, gradovi);
+});
 
 // Zadatak 6:
-// Napisati funkciju koja treba da primi kao parametar niz koji sadrzi parove naziva korisnika i njihovih korisnickih uloga,
-// npr: "markec|admin", "godzz|webmaster", "nikola123|admin", "nennad|moderator"
+document.getElementById("resultBtn6").addEventListener("click", function () {
+  let usersAndRoles = ["markec|admin", "godzz|webmaster", "nikola123|admin", "nennad|moderator"];
 
-// U funkciji se kreira drugi niz, koji sadrzi samo korisnicke uloge i to vraca kao povratnu vrednost. Napomena: ne dodavati duplikate.
+  function userRole(input) {
+    let roles = [];
+
+    for (let i in input) {
+      roles.push(input[i].split("|")[1]);
+    }
+    return new Set(roles);
+  }
+
+  let results = userRole(usersAndRoles);
+  console.log(results);
+});
 
 // Zadatak 7:
-// Napisati funkciju koja prima datu strukturu:
-// var korisnici = ["marko:17", "nikola:28", "pera:23", "nenad:15"];
+document.getElementById("resultBtn7").addEventListener("click", function () {
+  function isOver18(korisnici) {
+    for (let i in korisnici) {
+      let result = korisnici[i].split(":");
+      if (result[1] > 18) {
+        alert(`${result[0].toUpperCase()} - da`);
+      } else {
+        alert(`${result[0].toUpperCase()} - ne`);
+      }
+    }
+  }
 
-// Funkcija treba da prodje kroz zadati niz i da ispise da li je osoba punoletna ili ne.
-// Primer izvrsavanja:
-// Marko - Ne
-// Nikola - Da
-// Pera - Da
-// Nenad - Ne
+  let korisnici = ["marko:17", "nikola:28", "pera:23", "nenad:15"];
+  isOver18(korisnici);
+});
