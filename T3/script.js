affe02dc54aab9042502ee426357ef188a43c779
SlobodanDodic <slobodandodic@gmail.com>
2024-01-16 15:46:08 +0100

    JS/T3-homework

--- T3/script.js
+++ T3/script.js
@@ -1,4 +1,3 @@
-// document.write("<p>" + "Happy birthday dear" + " " + name + "</p>");
 // Zadatak 1:
 document.getElementById("resultBtn1").addEventListener("click", function () {
   let users = [];
@@ -134,51 +133,179 @@ document.getElementById("resultBtn7").addEventListener("click", function () {
 });
 
 // Zadatak 8:
+// let paragraph = document.createElement("p");
+// paragraph.textContent = "Happy birthday";
+// document.getElementById("resultBtn8").appendChild(paragraph);
+document.getElementById("resultBtn8").addEventListener("click", function () {
+  let x = prompt("Enter the number between 1 and 6:");
 
-// 1.	Kreirati funkciju koja ce za zadani broj od 1 - 6 kreirati HTML naslove do prosledjenog nivoa. Elemente kreirati uz
-// 	pomoc for petlje i ispisati u HTML dokument pomocu document.write() funkcije.
-// 		PRIMER: Ako je funkciji prosledjen broj 3, treba da se izgenerisu naslovi od h1 do h3.
-// 		REZULTAT:
-// 			<h1>Ovo je generisani naslov</h1>
-// 			<h2>Ovo je generisani naslov</h2>
-// 			<h3>Ovo je generisani naslov</h2>
+  function generateTitles(y) {
+    y = parseInt(y);
 
-// 2.	Izmeniti funkciju iz 1. zadatka tako da kao parametar prima 2 broja i generise naslove izmedju ta dva broja.
-// 		PRIMER: Ako su funkciju prosledjeni brojevi 2 i 4, generisu se naslovi od h2 do h4.
-// 		REZULTAT:
-// 			<h2>Ovo je generisani naslov</h2>
-// 			<h3>Ovo je generisani naslov</h3>
-// 			<h4>Ovo je generisani naslov</h4>
+    if (!isNaN(y) && typeof y === "number" && y <= 6 && y > 0) {
+      for (let i = 1; i <= y; i++) {
+        document.write(`<h${i}>` + "Ovo je generisani naslov" + `</h${i}>`);
+      }
+    } else {
+      alert("Invalid input. Please enter a number between 1 and 6.");
+    }
+  }
 
-// 3.	Kreirati funkciju koja ce uz oslonac na ugradjenu funkciju 'indexOf' proveravati da li prvi prosledjeni string sadrzi
-// 	drugi.
-// 		PRIMER: Ako su funkciji prosledjeni stringovi 'Dobar dan' i 'dan', funkcija treba da vrati vrednost true,
-// 		jer je string 'dan' sadrzan u stringu 'Dobar dan'.
+  generateTitles(x);
+});
 
-// 4.	Izmeniti funkciju iz 3. zadatka, tako da se ignorisu velika i mala slova prilikom provere.
+// Zadatak 9:
+document.getElementById("resultBtn9").addEventListener("click", function () {
+  let x = prompt("Enter the number between 1 and 6:");
+  let y = prompt("Enter the number between 1 and 6:");
 
-// 5.	Kreirati program koji analizira JMBG koji je definisan kao string (kao u primerima '10. numbers' i '11. string').
-// 	Znacajne delove JMBG-a izdvojiti putem funkcije substring.
-// 	JMBG je broj formata 'DDMMGGGRRBBBK' i sastoji se od sledecih informacija:
+  function generateTitles(param1, param2) {
+    let parsedParam1 = parseInt(param1);
+    let parsedParam2 = parseInt(param2);
 
-// 		DD - dan rođenja
-// 		MM - mesec rođenja
-// 		GGG - zadnje tri cifre godine rođenja
-// 		RR - region rođenja, odn. prebivalište za građane rođene pre 1976. godine.
-// 		BBB - pol:
-// 		    	000-499 - muški
-// 		    	500-999 - ženski
-// 		K - kontrolna cifra
+    let conditionOne = typeof parsedParam1 === "number" && parsedParam1 <= 6 && parsedParam1 > 0;
+    let conditionTwo = typeof parsedParam2 === "number" && parsedParam2 <= 6 && parsedParam2 > 0;
 
-// 	U zadatku ispisati datum rodjenja i pol osobe za zadani JMBG.
+    if (conditionOne) {
+      if (conditionTwo) {
+        for (let i = param1; i <= param2; i++) {
+          document.write(`<h${i}>` + "Ovo je generisani naslov" + `</h${i}>`);
+        }
+        // Option with writing in the same page:
+        // let resultContainer = document.getElementById("resultContainer9");
+        // let htmlContent = "";
+        // for (let i = param1; i <= param2; i++) {
+        //   htmlContent += `<h${i}>` + "Ovo je generisani naslov" + `</h${i}> `;
+        // }
+        // resultContainer.innerHTML = htmlContent;
+      } else {
+        alert(
+          `Invalid input for the second number. It must be between 1 and 6.\n
+          Your input was: ${param2}`
+        );
+      }
+    } else {
+      alert(
+        `Invalid input for the first number. It must be between 1 and 6.\n
+        Your input was: ${param1}`
+      );
+    }
+  }
 
-// 6.	Uz pomoc funkcije split() kreirati funckiju koja ce za zadati deo URL-a izvuci vrednosti HTTP GET parametara.
-// 	PRIMER:	Zadat je deo urla: '?ime=sinisa&prezime=mihajlovic'
-// 	REZULTAT: Rezultat je sledeci ispis u HTML dokument:
-// 		ime=sinisa
-// 		prezime=mihajlovic
+  generateTitles(x, y);
+});
 
-// 7.	Kreirati JavaScript program koji u HTML dokument ispisuje koliko je dana ostalo do vikenda u odnosu na trenutni dan.
-// 	Napomena: Trenutni dan mozete dobiti na sledeci nacin:
-// 	var danas = new Date();
-// 	var dan = danas.getDay(); // 0 - nedelja, 6 - subota
+// Zadatak 10:
+document.getElementById("resultBtn10").addEventListener("click", function () {
+  let stringOneInput = prompt("Enter the first string:");
+  let stringTwoInput = prompt("Enter the second string:");
+
+  function isContained(stringOne, stringTwo) {
+    let splitStringOne = stringOne.split(" ");
+
+    if (splitStringOne.indexOf(stringTwo) !== -1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  let result = isContained(stringOneInput, stringTwoInput);
+  alert(result);
+});
+
+// Zadatak 11:
+document.getElementById("resultBtn11").addEventListener("click", function () {
+  let stringOneInput = prompt("Enter the first string:");
+  let stringTwoInput = prompt("Enter the second string:");
+
+  function isContained(stringOne, stringTwo) {
+    let lowerStringOne = stringOne.toLowerCase();
+    let splitStringOne = lowerStringOne.split(" ");
+    stringTwo = stringTwo.toLowerCase();
+
+    if (splitStringOne.indexOf(stringTwo) !== -1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  let result = isContained(stringOneInput, stringTwoInput);
+  alert(result);
+});
+
+// Zadatak 12:
+document.getElementById("resultBtn12").addEventListener("click", function () {
+  let jmbg = prompt("Enter the JMBG:");
+
+  function isContained(jmbg) {
+    let condition = /^[0-9]+$/.test(jmbg) && jmbg.length === 13;
+
+    if (condition) {
+      let dd = jmbg.substring(0, 2);
+      let mm = jmbg.substring(2, 4);
+      let ggg = jmbg.substring(4, 7);
+
+      let bbb = "";
+      if (jmbg.substring(9, jmbg.length - 1) >= 500) {
+        bbb = "Female";
+      } else {
+        bbb = "Male";
+      }
+
+      alert(`
+        Date of birth: ${dd}.${mm}.1${ggg}.\n
+        Gender: ${bbb}\n
+        `);
+    } else if (jmbg.length !== 13) {
+      alert(
+        `Invalid input for the JMBG!\n
+        The number must have exactly 13 digits.\n
+        Your input had ${jmbg.length} digits`
+      );
+    } else {
+      alert(
+        `Invalid input for the JMBG!\n
+        JMBG must contain only numbers.\n
+        Your input was: ${jmbg}`
+      );
+    }
+  }
+
+  isContained(jmbg);
+});
+
+// Zadatak 13:
+document.getElementById("resultBtn13").addEventListener("click", function () {
+  let stringOneInput = prompt("Enter the first string:");
+
+  function getParameters(param) {
+    let splitParam = param.split("&");
+    let resultContainer = document.getElementById("resultContainer13");
+
+    resultContainer.innerHTML = `
+    <p>${splitParam[0].substring(1)}</p>\n
+    <p>${splitParam[1]}</p>
+  `;
+  }
+
+  getParameters(stringOneInput);
+});
+
+// Zadatak 14:
+document.getElementById("resultBtn14").addEventListener("click", function () {
+  let today = new Date();
+
+  function getWorkdays(param) {
+    let day = param.getDay();
+    let daysToWeekend = 5 - day;
+    let resultContainer = document.getElementById("resultContainer14");
+
+    resultContainer.innerHTML = `
+      <p>Days to weekend: ${daysToWeekend} day(s)</p>
+    `;
+  }
+
+  getWorkdays(today);
+});
