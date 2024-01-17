a7f6c882ab1b831ef86ffefcd7ccddc593b8e9e7
SlobodanDodic <slobodandodic@gmail.com>
2024-01-18 00:47:17 +0100

    JS/T4 - part 2

--- T4/FourthTask/js/script.js
+++ T4/FourthTask/js/script.js
@@ -1,67 +1,115 @@
-// Zadatak 0:
-// document.getElementById("resultBtn0").addEventListener("click", function () {
-//   let hoteli = [
-//     {
-//       naziv: "Hotel Park",
-//       adresa: "Novosadskog sajma 35",
-//       brojSoba: 140,
-//       rezervisano: 57,
-//       teretana: true,
-//       brojSlobodnihSoba: function () {
-//         let slobodno = this.brojSoba - this.rezervisano;
-//         return slobodno;
-//       },
-//       reservation: function () {
-//         this.rezervisano++;
-//       },
-//     },
-//     {
-//       naziv: "Centar",
-//       adresa: "Uspenska 1",
-//       brojSoba: 90,
-//       rezervisano: 68,
-//       teretana: false,
-//       brojSlobodnihSoba: function () {
-//         let slobodno = this.brojSoba - this.rezervisano;
-//         return slobodno;
-//       },
-//     },
-//   ];
+//  Q1:
+//  - Kako napraviti da btn bude disabled i proveriti da li je unos user-a tacan.
+//    const submitBtn = document.getElementById("submitBtn");
+//    submitBtn.disabled = true;
 
-//   console.log(hoteli[0].rezervisano);
-//   console.log(hoteli[0].reservation());
-
-//   for (let i = 0; i < hoteli.length; i++) {
-//     let hotel = hoteli[i];
-//     console.log(hoteli[i]);
-
-//     document.write("<br><b>Podaci o hotelu:</b><br>");
-//     document.write("Naziv: " + hotel.naziv + "<br>");
-//     document.write("Adresa: " + hotel.adresa + "<br>");
-//     document.write("Kapacitet: " + hotel.brojSoba + "<br>");
-//     document.write("Teretana: ");
-
-//     if (hotel.teretana == true) {
-//       document.write("DA<br>");
-//     } else {
-//       document.write("NE<br>");
-//     }
-
-//     document.write("Broj slobodnih soba: " + hotel.brojSlobodnihSoba() + "<br>");
-//   }
-// });
+//  Q2:
+//  - Da li ipak dovoljno: document.getElementById("eco").style.visibility = "visible";
+//  function ecoClassOn() {
+//    let eco = document.getElementById("eco");
+//    if (eco.className == "eco") {
+//      document.getElementById("eco").style.visibility = "visible";
+//    }
+//  }
 
 // Zadatak 4
-// 	4.1. Proveriti da li su sva polja forme popunjena. Ukoliko sva polja forme nisu popunjena
-// 	zabraniti submit forme i prebaciti fokus na polje koje nije popunjeno.
+// 1. Proveriti da li su sva polja forme popunjena.
+// 2. Ukoliko sva polja forme nisu popunjena zabraniti submit forme i prebaciti fokus na polje koje nije popunjeno.
+// 3. Proveriti da li su pocetna slova imena i prezimena napisana velikim slovom i da li JMBG ima 13 brojeva.
+// 4. Napraviti da se paragraf "Eko klasa" prikazuje prilikom ulaska kursora u taj red Tip vozila, a da se sakriva prilikom izlaska.
+// 5. Ukoliko je odabran elektricni tip vozila obojiti paragraf "Eko klasa" zelenom bojom, odnosno crvenom bojom ako je motorni tip vozila.
+// 6. Preuzeti parametre forme iz index.html i uneti ih u tabelu table.html
 
-// 	4.2. Proveriti da li su pocetna slova imena i prezimena napisana velikim slovom i da li JMBG ima 13 brojeva.
+if (window.location.pathname === "/T4/FourthTask/table.html") {
+  if (!window.location.search) {
+    window.history.back();
+  } else {
+    let table = document.getElementById("table");
 
-// 	4.3. Red Tip vozila ima combo box sa dve vrednosti: "Motorno" i "Elektricno",
-// napraviti tako da se paragraf "Eko klasa" prikazuje prilikom ulaska kursora u taj red,
-// a da se sakriva prilikom njegovog izlaska
+    let search = window.location.search;
+    let removeFirstChar = search.substring(1);
+    let splitSearch = removeFirstChar.split("&");
 
-// 	4.4. Ukoliko je odabran elektricni tip vozila obojiti paragraf "Eko klasa" zelenom bojom,
-// ukoliko je odabran motorni tip vozila obojiti isti paragraf crvenom bojom
+    let name = splitSearch[0].split("=")[1];
+    let lastName = splitSearch[1].split("=")[1];
+    let jmbg = splitSearch[2].split("=")[1];
 
-// 	4.5. Preuzeti parametre forme iz Zadatak1.html i uneti ih u tabelu Zadatak2.html
+    if (table) {
+      let tableStorage = JSON.parse(localStorage.getItem("table")) || [];
+
+      if (tableStorage.length > 0) {
+        for (let i in tableStorage) {
+          let newRow = document.createElement("tr");
+          newRow.innerHTML = `<td> ${tableStorage[i].name} </td><td> ${tableStorage[i].lastName} </td><td> ${tableStorage[i].jmbg} </td>`;
+          table.appendChild(newRow);
+        }
+      }
+
+      let newRow = document.createElement("tr");
+      newRow.innerHTML = `<td> ${name} </td><td> ${lastName} </td><td> ${jmbg} </td>`;
+      table.appendChild(newRow);
+
+      tableStorage.push({
+        name: name,
+        lastName: lastName,
+        jmbg: jmbg,
+      });
+
+      localStorage.setItem("table", JSON.stringify(tableStorage));
+    } else {
+      console.error("Table element not found");
+    }
+  }
+}
+
+function selectedType(type) {
+  if (type.value === "2") {
+    document.getElementById("eco").style.color = "green";
+  } else {
+    document.getElementById("eco").style.color = "red";
+  }
+}
+
+function ecoClassOn() {
+  document.getElementById("eco").style.visibility = "visible";
+}
+
+function ecoClassOff() {
+  document.getElementById("eco").style.visibility = "hidden";
+}
+
+function proveraPolja(polje, tekst, jmbg = false) {
+  if (polje.value === "") {
+    alert(tekst);
+    polje.focus();
+    return false;
+  }
+
+  if (jmbg) {
+    let jmbgCondition = /^[0-9]+$/.test(polje.value) && polje.value.length === 13;
+    if (!jmbgCondition) {
+      alert(`Invalid input for the JMBG!\nJMBG must have exactly 13 digits and must contain only numbers.`);
+      polje.focus();
+      return false;
+    }
+  }
+
+  if (polje.value[0] !== polje.value[0].toUpperCase()) {
+    alert("The first letter must be uppercase.");
+    polje.focus();
+    return false;
+  }
+
+  return true;
+}
+
+function proveraForme(form) {
+  return (
+    proveraPolja(form.ime, "Niste uneli ime") &&
+    proveraPolja(form.ime, "Ime mora poceti velikim slovom!") &&
+    proveraPolja(form.prezime, "Niste uneli prezime") &&
+    proveraPolja(form.prezime, "Prezime mora poceti velikim slovom!") &&
+    proveraPolja(form.jmbg, "Niste uneli JMBG", true) &&
+    proveraPolja(form.registracija, "Niste uneli registraciju")
+  );
+}
