ea441f0d3e5df9b4d9d937e4a49f74207d5b1242
SlobodanDodic <slobodandodic@gmail.com>
2024-01-22 13:38:43 +0100

    JS/T6 - script-p2

--- T6/js/script-p2.js
+++ T6/js/script-p2.js
@@ -8,10 +8,40 @@
 
 // Kreirate objekat Email poruka koji nasledjuje poruku.
 // Od dodatnih atributa ima email adrese posiljaoca i primaoca i redefinise metodu stampaj tako da ukljucuje pomenute atribute.
-// Pored toga ima jos jednu metodu koja se zove potpisPosiljaoca i koja ima jedan parametar koji ce biti ispisan kao potpid.
+// Pored toga ima jos jednu metodu koja se zove potpisPosiljaoca i koja ima jedan parametar koji ce biti ispisan kao potpis.
 
-const poruka = {
-  posiljaoc: "",
-  primaoc: "",
-  poruka: "",
+let Poruka = {
+  posiljaoc: "Poslanko",
+  primaoc: "Primalac",
+  poruka: "Moja poruka",
+  stampaj: function () {
+    document.write(`Poruka: ${this.posiljaoc} ${this.primaoc} ${this.poruka} <br />`);
+  },
 };
+
+let SMSPoruka = Object.create(Poruka);
+SMSPoruka.telefon = "067 5147 355";
+
+SMSPoruka.stampaj = function () {
+  Poruka.stampaj.call(this);
+  document.write(`SMSPoruka: ${this.posiljaoc} ${this.primaoc} ${this.poruka} ${this.telefon}<br />`);
+};
+
+let Email = Object.create(Poruka);
+SMSPoruka.adresaPosiljaoca = "adresa posiljaoca";
+SMSPoruka.adresaPrimaoca = "adresa primaoca";
+
+SMSPoruka.stampaj = function () {
+  Poruka.stampaj.call(this);
+  document.write(
+    `SMSPoruka: ${this.posiljaoc} ${this.primaoc} ${this.poruka} ${this.telefon} ${this.adresaPosiljaoca} ${this.adresaPrimaoca}<br />`
+  );
+};
+
+SMSPoruka.potpisPosiljaoca = function (potpis) {
+  document.write(`Potpis posiljaoca: ${potpis} <br />`);
+};
+
+Poruka.stampaj();
+SMSPoruka.stampaj();
+SMSPoruka.potpisPosiljaoca("Slobodan");
